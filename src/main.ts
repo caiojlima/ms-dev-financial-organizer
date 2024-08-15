e412048cc51524fa8ce6bf1a35590f847ec7e0c9
Caio Lima <caiojlimah@gmail.com>
2024-08-15 20:46:29 -0300

    [SETUP]: Updating CORS

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   }))
 
   app.enableCors({
-    origin: '*',
+    origin: process.env.APP_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
